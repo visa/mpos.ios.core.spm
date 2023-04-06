162138258167b3c968e89def2e24c7e32a8dde58
svc_munich-lab-ci <remgroup@visa.com>
2023-04-06 14:37:06 +0200

    Update mpos.ios.sdk version: 2.59.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
-// Copyright © 2021 Apple Inc. and the Swift project authors
-// Copyright © 2023 VISA Inc.
 // swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+// Copyright © 2021 Apple Inc. and the Swift project authors
+// Copyright © 2023 VISA Inc.
 
 import PackageDescription
 
